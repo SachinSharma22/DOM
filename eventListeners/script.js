2f11d5ea9006fd7a806b63cea655cce005e008e1
SachinSharma22 <s830sharma@gmail.com>
2025-03-24 17:19:16 +0530

    prevent default behaviour of anchor element

--- eventListeners/script.js
+++ eventListeners/script.js
@@ -1,14 +1,24 @@
 // let fpara = document.getElementById('fpara');
 // fpara.textContent = "sachin";
 
-function changeText(event) {
-    console.log(event)
-    let fpara = document.getElementById('fpara');
-    fpara.textContent = "Your eventlistner perfome well";
-}
+// function changeText(event) {
+//     console.log(event)
+//     let fpara = document.getElementById('fpara');
+//     fpara.textContent = "Your eventlistner perfome well";
+// }
 
-let fpara = document.getElementById('fpara');
-fpara.addEventListener('click', changeText);
+// let fpara = document.getElementById('fpara');
+// fpara.addEventListener('click', changeText);
 
 // If you want to stop eventListner then just writ removeEventlistner('click', your function)
 // fpara.removeEventListener('click', changeText); 
+
+// Here we try to change the default behaviour of anchor tag
+let anchorTag  = document.getElementById('fanchor');
+
+
+anchorTag.addEventListener('click', function(event) {
+    event.preventDefault();
+    anchorTag.textContent = "Click done";
+});
+
