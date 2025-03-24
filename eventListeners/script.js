31a7f9474a7a0fe60eaa9fd5f71aee9507143210
SachinSharma22 <s830sharma@gmail.com>
2025-03-24 17:57:56 +0530

    doing same task using one parent node

--- eventListeners/script.js
+++ eventListeners/script.js
@@ -22,3 +22,29 @@
 //     anchorTag.textContent = "Click done";
 // });
 
+
+// here we use a method to call several addEventListener at once
+// let paras = document.querySelectorAll('p');
+
+// function alertMsg(event) {
+    
+//         alert("You hava clicked on para: "+ event.target.textContent);
+// }
+
+// for(let i = 0; i<paras.length; i++){
+//     let para = paras[i];
+//     para.addEventListener('click', alertMsg);
+// }
+
+
+// This is another approach for doing same task
+
+let mydiv = document.getElementById('wrapper');
+
+
+// Make sure you only clicked on the element because when you clicked on other part of the document JavaScript alerts you that you have clicked on document
+function alertMsg (event) {
+    alert("You have clicked on : "+ event.target.textContent);
+}
+
+document.addEventListener('click', alertMsg);
