c045cf6246394f554dcbe3728510ace601e51234
SachinSharma22 <s830sharma@gmail.com>
2025-03-26 17:33:44 +0530

    Reflow and Repaint method

---
+++ ReflowandRepaint/script.js
@@ -0,0 +1,32 @@
+// method 1
+// THis methos is called Reflow Method
+// It tooks much time then Repaint because when this event is happend it reload the whole document
+
+let p1 = performance.now();
+for(let i=0;i<100;i++){
+    let para = document.createElement('p');
+    para.textContent = "This is para:" + i;
+
+    document.body.append(para);
+}
+let p2 = performance.now();
+console.log("Time is :"+ (p2 -p1));
+
+
+
+// Method 2
+// You can see on console that second method tooks very few time to do the task
+// This method is called Rapaint Method in terms of performence
+let p3 = performance.now();
+
+let myDiv = document.createElement('div');
+for(let i = 0; i<100;i++){
+    let para2 = document.createElement('p');
+    para2.textContent = "This is div Para " + i;
+    myDiv.appendChild(para2);
+}
+
+document.body.append(myDiv);
+
+let p4 = performance.now();
+console.log("This time is "+ (p4 -p3));
