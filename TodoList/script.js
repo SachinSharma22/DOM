da23829bfbdd0c85527cf29844fdb9fce41d7a62
SachinSharma22 <s830sharma@gmail.com>
2025-03-27 17:51:28 +0530

    write function to add item in the list

--- TodoList/script.js
+++ TodoList/script.js
@@ -1,1 +1,18 @@
+console.log("Everything is good");
+
 const inputTag = document.getElementById('input');
+const addBtn = document.getElementById('button');
+
+console.log("next is you function");
+
+function addListItem() {
+    console.log("function works");
+    let ulTag = document.getElementById('item-container');
+    let liItem = document.createElement('li');
+    // console.log(liItem);
+    liItem.textContent = inputTag.value;
+    console.log(liItem.textContent);
+    ulTag.appendChild(liItem);
+}
+
+addBtn.addEventListener('click', addListItem);
