13a52f091d394b3d9097688d329312988dc5d6c6
SachinSharma22 <s830sharma@gmail.com>
2025-03-27 14:46:29 +0530

    setTimeout() function

---
+++ eventLoop/script.js
@@ -0,0 +1,43 @@
+//Synchronous code in JavaScript 
+// This is ByDdefault
+console.log("Hiii");
+
+function greet() {
+    console.log("Sachin Sharma");
+}
+
+greet();
+
+console.log("Welcome");
+
+// Asynchronous Code in JavaScript
+
+function fetchData(callback) {
+    setTimeout(() => {
+        const data = "Data fetched!";
+        callback(data);
+    },2000);
+}
+
+fetchData ((data) => {
+    console.log(data);
+})
+
+console.log("start");
+
+setTimeout(() => {
+    console.log("This is delayed by 2 seconds!");
+},2000);
+
+console.log("End");
+
+//Cancel the timeout before it triggers
+
+ const timeoutId = setTimeout(() =>{
+    console.log("This will not run");
+ },2000);
+
+ clearTimeout(timeoutId);
+
+
+ //Event loop 
