6c836fe8c3ac141b29c078de473500c684741463
SachinSharma22 <s830sharma@gmail.com>
2025-03-27 18:08:28 +0530

    add function' exit ;

--- TodoList/script.js
+++ TodoList/script.js
@@ -1,18 +1,16 @@
-console.log("Everything is good");
-
 const inputTag = document.getElementById('input');
 const addBtn = document.getElementById('button');
 
-console.log("next is you function");
-
 function addListItem() {
-    console.log("function works");
-    let ulTag = document.getElementById('item-container');
+    const ulTag = document.getElementById('item-container');
     let liItem = document.createElement('li');
-    // console.log(liItem);
     liItem.textContent = inputTag.value;
-    console.log(liItem.textContent);
-    ulTag.appendChild(liItem);
+    let deleteBtn = document.createElement('button');
+    deleteBtn.innerText = "Delete";
+    deleteBtn.id = "jsBtn";
+
+    ulTag.append(liItem, deleteBtn);
+
 }
 
 addBtn.addEventListener('click', addListItem);
