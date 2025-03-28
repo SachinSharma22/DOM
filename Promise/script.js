902f1c122421cb9ea7d4ed0cea868fbf785dd15d
SachinSharma22 <s830sharma@gmail.com>
2025-03-28 14:23:54 +0530

    promise.all method

--- Promise/script.js
+++ Promise/script.js
@@ -1,3 +1,4 @@
+/*
 let promise1 = new Promise((resolve, reject) => {
     let success =  true;
 
@@ -22,6 +23,27 @@ promise1.then((message) => {
 }).finally((message) =>{
     console.log("I will run defenetilly ");
 })
+    */
+
+// Handling multiple Promise at the same time
+
+let promise1 = new Promise((resolve, reject) =>{
+    setTimeout(resolve, 1000, "First");
+})
+let promise2 = new Promise((resolve, reject) =>{
+    setTimeout(resolve, 2000, "second");
+})
+let promise3 = new Promise((resolve, reject) =>{
+    setTimeout(reject, 3000, "Third");
+})
+
+Promise.all([promise1, promise2, promise3]).then((value) => {
+    console.log(value);
+})
+.catch((error) => {
+    console.log(error);
+})
+
 
 
 
