cd1e85e1fc2a25f4414768300413673c73418014
SachinSharma22 <s830sharma@gmail.com>
2025-03-28 13:11:39 +0530

    comment all code

--- Promise/script.js
+++ Promise/script.js
@@ -1,3 +1,4 @@
+/*
 function doSomething () {
     return new Promise((resolve) =>{
         setTimeout(() => {
@@ -11,3 +12,51 @@ function doSomething () {
 }
 
 doSomething();
+
+
+let myPromise = new Promise(function(resolve, reject) {
+    let success = true;
+
+    if(success) {
+        resolve("Operation successful!");
+    } else {
+        reject("Operation failed!");
+    }
+});
+
+myPromise
+.then(result => {
+    console.log(result);
+})
+.catch(error => {
+    console.log(error);
+});
+
+// Chaining Promises
+
+myPromise
+.then(result =>{
+    console.log(result);
+    return "next step";//passing the value to the next .then parameter
+})
+.then(nextResult =>{
+    console.log(nextResult);
+})
+.catch(error =>{
+    console.log(error);
+});
+
+// async operation
+
+function fetchData() {
+    return new Promise((resolve, reject) =>{
+        setTimeout(() =>{
+            const data = "Data factch successfully!";
+            resolve(data);
+        }, 2000);
+    });
+}
+
+fetchData()
+.then(result => console.log(result))
+.catch(error => console.log(error));*/
--- Weather/script.js
+++ Weather/script.js
@@ -5,10 +5,21 @@ async function fetchWeatherDetail() {
 
     //convert data into JSON formate
     const data = await response.json();
-    console.log("Weather data:-> " + data);
+    console.log("Weather data:-> " , data);
 
     let newPara = document.createElement('p');
     newPara.textContent = `${data?.main?.temp.toFixed(2)} C`;
 
     document.body.appendChild(newPara);
+}
+
+async function getCustomWeatherDetail() {
+    let latitude = 15.6333;
+    let longitude = 18.3333;
+    let result = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${APIKey}&units=matric`);
+
+    const data2 = await result.json();
+    console.log("Weather Data:-> ", data2);
+    console.log(data2);
+    
 }
